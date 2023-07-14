8eb43763271d62a5334e7159f6eeb72004527fcd
Anamika Jha <89740849+anamiikajha@users.noreply.github.com>
2023-07-14 19:20:53 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import './App.css';
+import "./style.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+    return (
+      <div className="calculator-grid">
+        <div className="output">
+            <div className="previous-operand"></div>   
+            <div className="current-operand"></div>
+        </div>
+        <button className="span-two">AC</button>
+        <button>DEL</button>
+        <button>÷</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>×</button>
+        <button>4</button>        
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className="span-name">=</button>
+        
+        
+  
+        
+        </div>
+   
+   
+        )
 }
 
-export default App;
+export default App
